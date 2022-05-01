349cb990413e91611eb73bf7076d2b31ca2b1199
Sergi <90915962+SergiMasip@users.noreply.github.com>
2022-05-01 19:23:55 +0200

    yesssss

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function parrafos() {
+    var infonum = document.getElementById('p1')
+    var infonum2 = document.getElementById('p2')
+    var numero = document.getElementById('numero').value
+    var calculando = document.getElementById('calculo')
+    calculando.innerHTML = parseInt(numero, 2);
+    var ultimodigito = numero.charAt(numero.length - 1)
+    if (numero == '') {
+        calculando.innerHTML = ''
+    }
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        infonum2.innerHTML = 'Here is your decimal'
+        infonum.innerHTML = ''
+    } else {
+        calculando.innerHTML = 'Waiting for a valid binary number...'
+    }
+    if (ultimodigito >> 1 || ultimodigito >> 9999999) {
+        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+        infonum2.innerHTML = ''
+    }
+    if (ultimodigito == '') {
+        infonum.innerHTML = ''
+        infonum2.innerHTML = ''
+    }
+}
